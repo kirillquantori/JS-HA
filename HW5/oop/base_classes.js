d66ea529a5606e1fa02f654249c23c65acdc2930
kirillquantori <kirill.piksendeev@quantori.academy>
2023-04-19 21:31:09 +0300

    First version

--- HW5/oop/base_classes.js
+++ HW5/oop/base_classes.js
@@ -4,30 +4,33 @@ class Component {
         this.props = {};
         this.element = document.createElement('div');
     }
-
     setState(state) {
         this.state = {...this.state, ...state};
         this.update();
     }
-
-    /**
-     *
-     * @param props
-     * @returns {HTMLElement}
-     */
     render(props = {}) {
         this.props = {...props};
         const div = this.element;
+        div.classList = props.class;
         div.onclick = props.onClick;
+        div.placeholder = props.placeholder;
+        div.type = props.type;
+        div.id = props.id;
+        div.oninput = props.onInput;
+        div.onchange = props.onChange;
+        div.checked = props.checked;
         if (props.style) {
             div.style = props.style;
         }
-        div.innerHTML = '';
+        props.value ? div.value = props.value : '';
+        props.onSearch ? div.onsearch = props.onSearch: '';
+        props.htmltext ? div.innerHTML = props.htmltext : div.innerHTML = '';
         div.append(...props.children)
+
         return div;
     }
-
     update() {
         this.render(this.props);
+        document.getElementById("SearchString") ? document.getElementById("SearchString").oninput.apply(): '';
     }
 }
